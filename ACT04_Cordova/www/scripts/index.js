4b62394848ea03f4ac7a35effeb058c5e16bddfc
Matias <matias.salgado4@gmail.com>
2017-10-21 20:39:14 -0300

    code update

--- ACT04_Cordova/www/scripts/index.js
+++ ACT04_Cordova/www/scripts/index.js
@@ -1,61 +1,61 @@
 ﻿/*
-    Con esta sintaxis se puede emular el funcionamiento de clases en JS previo a
-    ES6
-
-    MusicPlayer una clase que relaciona y almacena todos los sonidos por su nombre descriptivo
+    La clase App tiene similitud a la implementacion tipica de una actividad en android nativo
+    los eventos tipicos del ciclo de vida se relacionan a un metodo lo que permite en una
+    estructura de codigo mucho mas ordena trabajar de una forma muy parecida al desarrollo nativo
  */
 
-function MusicPlayer() {
-    this.background = new Audio("rec/fondo.mp3");
-    this.croak = new Audio('rec/ranar.mp3');
-    this.croakb = new Audio('rec/ranav.mp3');
-    this.error = new Audio('rec/no.mp3');
-    this.drimm = new Audio('rec/drim.mp3');
+function APP(dom) {
+    /**
+     Referencia a al objeto mismo(var _this), utilizada en funciones llamadas en callbacks debido al cambio de scope
+     https://www.w3schools.com/js/js_scope.asp
+     */
+    console.log("._.");
+    var _this = this;
+    this.doc = $(dom);
 
-    this.background.loop = true;
-    this.background.play();
+    this.loadAndroidEvents = function () {
+        /*
+            Ya que el ciclo de vida de android es relativamente complejo y extenso no se implementan cada uno de los eventos
+            sino solo los necesarios, si la aplicacion requiriese reconexiones, inicializar sockets, o utilizacion de sensores
+            o hardware del telefono se deberia implementar cada uno de estos metodos de acuerdo al caso.
+            Aqui no es necesario
+        */
+        //_this.doc.on('create', _this.onCreate);
+        //_this.doc.on('start', _this.onStart);
+        //_this.doc.on('restart', _this.onRestart);
+        //_this.doc.on('resume', _this.onResume);
+        //_this.doc.on('pause', _this.onPause);
+        //_this.doc.on('stop', _this.onStop);
+        //_this.doc.on('destroy', _this.onDestroy);
+    }
+    
+    this.onCreate = function () {
+        console.log("Create event");
+    }
+
+    this.onStart = function () {
+    }
+
+    this.onRestart = function () {
+    }
+
+    this.onResume = function () {
+        console.log("Pause Event");
+    }
+
+    this.onPause = function () {
+    }
+
+    this.onStop = function () {
+    }
+
+    this.onDestroy = function () {
+    }
+
+    this.doc.ready(this.loadAndroidEvents);
+    console.log("app created");  
 
     return this;
 }
 
-function App() {
-
-    this.onCreate();
-
-    this.onStart();
-
-    this.onRestart();
-
-    this.onResume();
-
-    this.onPause();
-
-    this.onStop();
-
-    this.onDestroy();
-    return this;
-}
-(function () {
-    "use strict";
-
-    // Aqui se aplican los distintos eventos del ciclo de vida tipico de una app
-    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener('pause', onPause.bind(this), false);
-        document.addEventListener('resume', onResume.bind(this), false);
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-
-        
-    };
-
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+app = new APP(document);
---
+++ ACT04_Cordova/www/scripts/musicPlayer.js
@@ -0,0 +1,27 @@
+﻿/*
+    Con esta sintaxis se puede emular el funcionamiento de clases en JS previo a
+    ES6. Como este framework utiliza las WebViews de los sistemas moviles a dia de hoy
+    (Octubre 2017) lo mas probable es que no este adoptado este estandar en dichos componentes.
+    Mas info http://es6-features.org/#ClassDefinition
+
+    MusicPlayer una clase que relaciona y almacena todos los sonidos por su nombre descriptivo
+    similar a un struct de C o una clase de solo atributos publicos.
+
+    A pesar que es un juego simple preferi utilizar estructuras tipicas de motores de juego para
+    demostrar el potencial que tiene Apache Cordova a traves de elementos web de generar aplicaciones y
+    estructuras de software complejas a traves de una sintaxis no vista en los cursos de la universidad
+    y sin en frameworks adicionales a JQuery.
+ */
+
+function MusicPlayer() {
+    this.background = new Audio("../sound/fondo.mp3");
+    this.croak      = new Audio('../sound/ranar.mp3');
+    this.croakb     = new Audio('../sound/ranav.mp3');
+    this.error      = new Audio('../sound/no.mp3');
+    this.drimm      = new Audio('../sound/drim.mp3');
+
+    this.background.loop = true;
+    this.background.play();
+
+    return this;
+}
