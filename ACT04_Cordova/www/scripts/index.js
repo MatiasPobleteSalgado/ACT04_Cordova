ae0d816e4d5836426f0691cf6cf0fcfef3d890b1
Matias <matias.salgado4@gmail.com>
2017-10-21 17:59:50 -0300

    Se agrega estructura de aplicacion inicial y archivos de recursos multimedia

--- ACT04_Cordova/www/scripts/index.js
+++ ACT04_Cordova/www/scripts/index.js
@@ -1,23 +1,54 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
+﻿/*
+    Con esta sintaxis se puede emular el funcionamiento de clases en JS previo a
+    ES6
+
+    MusicPlayer una clase que relaciona y almacena todos los sonidos por su nombre descriptivo
+ */
+
+function MusicPlayer() {
+    this.background = new Audio("rec/fondo.mp3");
+    this.croak = new Audio('rec/ranar.mp3');
+    this.croakb = new Audio('rec/ranav.mp3');
+    this.error = new Audio('rec/no.mp3');
+    this.drimm = new Audio('rec/drim.mp3');
+
+    this.background.loop = true;
+    this.background.play();
+
+    return this;
+}
+
+function App() {
+
+    this.onCreate();
+
+    this.onStart();
+
+    this.onRestart();
+
+    this.onResume();
+
+    this.onPause();
+
+    this.onStop();
+
+    this.onDestroy();
+    return this;
+}
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    // Aqui se aplican los distintos eventos del ciclo de vida tipico de una app
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+
+        
     };
 
     function onPause() {
